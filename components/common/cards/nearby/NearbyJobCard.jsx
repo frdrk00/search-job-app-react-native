4fdbfc6cb37ac0563b2258fb2bbcf8b4c2230ebe
frdrk00 <masterkooo@gmail.com>
2023-06-12 23:23:50 +0200

    Display Content

--- components/common/cards/nearby/NearbyJobCard.jsx
+++ components/common/cards/nearby/NearbyJobCard.jsx
@@ -12,7 +12,7 @@ const NearbyJobCard = ({ job, handleNavigate }) => {
       <TouchableOpacity style={styles.logoContainer}>
         <Image
           source={{
-            uri: checkImageURL(job.employer_log)
+            uri: checkImageURL(job.employer_logo)
               ? job.employer_logo
               : 'https://t4.ftcdn.net/jpg/05/05/61/73/360_F_505617309_NN1CW7diNmGXJfMicpY9eXHKV4sqzO5H.jpg',
           }}
